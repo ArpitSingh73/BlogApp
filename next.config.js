460a36db0cc7cbfb3f891197003313db748689f5
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-04-16 09:33:25 +0530

    project init. again.

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
